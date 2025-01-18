4c43593aa6b92e122accd084f7893f8844277a44
aashish-nomad <aashish.nomad@gmail.com>
2025-01-18 09:20:43 +0545

    cleans S3Storage code

--- index.php
+++ index.php
@@ -3,11 +3,22 @@
 require 'vendor/autoload.php';
 
 use App\S3Storage;
+use Aws\S3\S3Client;
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$storage = new S3Storage();
+// Instantiate an Amazon S3 client.
+$client = new S3Client([
+  'version' => 'latest',
+  'region' => 'us-east-1',
+  'credentials' => [
+    'key' => $_ENV['AWS_KEY'],
+    'secret' => $_ENV['AWS_SECRET'],
+  ],
+]);
+
+$storage = new S3Storage($client, $_ENV['AWS_BUCKET']);
 
 $storage->put('new.txt', 'Hello World');
--- src/S3Storage.php
+++ src/S3Storage.php
@@ -7,27 +7,15 @@ use Aws\S3\Exception\S3Exception;
 
 class S3Storage implements FileStorage
 {
+
+  public function __construct(protected S3Client $client, protected string $bucket) {}
+
   public function put(string $path, string $content): void
   {
-    // AWS credentials
-    $s3key = $_ENV['AWS_KEY'];
-    $s3Secret = $_ENV['AWS_SECRET'];
-    $bucket = $_ENV['AWS_BUCKET'];
-
-    // Instantiate an Amazon S3 client.
-    $s3 = new S3Client([
-      'version' => 'latest',
-      'region' => 'us-east-1',
-      'credentials' => [ // Fixed the typo here
-        'key' => $s3key,
-        'secret' => $s3Secret,
-      ],
-    ]);
-
     // Upload a publicly accessible file. The file size and type are determined by the SDK.
     try {
-      $result = $s3->putObject([
-        'Bucket' => $bucket,
+      $result = $this->client->putObject([
+        'Bucket' => $this->bucket,
         'Key'    => $path,
         'Body'   => $content,
       ]);
